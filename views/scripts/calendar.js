2c1183ad9b9b0b0160dc5ca667467fd4bc373141
jblackman42 <jblackman@pureheart.org>
2022-08-31 17:01:16 -0700

    popup shows events for each calendar dag

--- views/scripts/calendar.js
+++ views/scripts/calendar.js
@@ -1,6 +1,8 @@
 const calenderDOM = document.getElementById('calender');
 const dateLabel = document.getElementById('date-label');
 
+const maxEvents = 50;
+
 const getMonth = (year, month) => {
     const date = new Date(year, month, 1);
 
@@ -41,15 +43,20 @@ let currentMonth = today.getMonth();
 let dates = getMonth(currentYear, currentMonth)
 
 function drawCalender() {
-    dateLabel.innerHTML = `${toMonthName(currentMonth)} ${currentYear}`
+    dateLabel.innerHTML = `${toMonthName(currentMonth)} ${currentYear}`;
 
     let calenderHTML = '';
 
     for (var i=0; i < dates.length; i++) {
         const date = dates[i];
+
+        const eventFull = Math.floor((date.numberOfEvents / maxEvents) * 100);
+
         let dateHTML = `
-            <div class='calender-day ${dates[i].numberOfEvents >= 1 ? 'event' : ''}'>
+            <div class='calender-day ${date.numberOfEvents >= 1 ? 'event' : ''}' onClick='popup("${date.day}", ${date.numberOfEvents})'>
                 <p>${date.monthday}</p>
+                ${date.numberOfEvents > 0 ? `<p class='eventsNumber'>${date.numberOfEvents} ${date.numberOfEvents > 1 ? 'Events' : 'Event'}</p>` : ''}
+                <div class="progressBar" style="width: ${eventFull}%"></div>
             </div>
         `
         calenderHTML += dateHTML;
@@ -58,11 +65,15 @@ function drawCalender() {
 }
 drawCalender();
 
-function toMonthName(monthNumber) {
-    const date = new Date();
-    date.setMonth(monthNumber);
+function getEventsFromDay(day) {
+    // const daySimplified = day.toDateString();
 
-    return date.toLocaleString('en-US', {
-        month: 'long',
-    });
+    let daysEvents = events.filter(event => new Date(event.Event_Start_Date).toDateString() == day && !event.Cancelled)
+    
+    return daysEvents;
+}
+
+function toMonthName(monthNumber) {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return months[monthNumber]
 }
---
+++ views/scripts/ministryPlatform.js
@@ -0,0 +1,49 @@
+const getEvents = () => {
+    const response = axios({
+        method: 'get',
+        url: `https://my.pureheart.org/ministryplatformapi/tables/events?%24orderby=Event_Start_Date%20DESC${iteration > 0 ? `&%24skip=${600 * iteration}` : ''}`,
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${access_token}`
+        }
+    })
+    .then(response => response.data)
+    .catch(err => {
+        console.error(err)
+    })
+    return response;
+}
+
+const getEventRooms = (Event_ID) => {
+    if (!Event_ID) return
+    const response = axios({
+        method: 'get',
+        url: `https://my.pureheart.org/ministryplatformapi/tables/Event_Rooms?%24filter=Event_ID%3D${Event_ID}`,
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${access_token}`
+        }
+    })
+    .then(response => response.data)
+    .catch(err => {
+        console.error(err)
+    })
+    return response;
+}
+
+const getRoom = (Room_ID) => {
+    if (!Room_ID) return
+    const response = axios({
+        method: 'get',
+        url: `https://my.pureheart.org/ministryplatformapi/tables/Rooms?%24filter=Room_ID%3D${Room_ID}`,
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${access_token}`
+        }
+    })
+    .then(response => response.data[0])
+    .catch(err => {
+        console.error(err)
+    })
+    return response;
+}
