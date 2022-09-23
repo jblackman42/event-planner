ca592fa3889a1e13d109189aa39b9ad47b10bb24
jblackman42 <jblackman@pureheart.org>
2022-09-23 14:01:51 -0700

    recurring event month functionality

--- views/scripts/recurringEvent.js
+++ views/scripts/recurringEvent.js
@@ -4,7 +4,7 @@ const recurringEventContainer = document.querySelector('.recurring-event-contain
 const dailyTab = document.getElementById('daily');
 const weeklyTab = document.getElementById('weekly');
 const monthlyTab = document.getElementById('monthly');
-const yearlyTab = document.getElementById('yearly');
+// const yearlyTab = document.getElementById('yearly');
 
 const startByDate = document.getElementById('start-by');
 
@@ -34,7 +34,7 @@ const cancel = () => {
 }
 
 const updatePatternOption = () => {
-    const options = ['daily', 'weekly', 'monthly', 'yearly'];
+    const options = ['daily', 'weekly', 'monthly'];
     const activeOption = document.querySelector('.active').id;
 
     const hiddenOptions = options.filter(option => option != activeOption);
@@ -53,7 +53,6 @@ const showDaily = () => {
     dailyTab.classList.add('active')
     weeklyTab.classList.remove('active');
     monthlyTab.classList.remove('active');
-    yearlyTab.classList.remove('active');
     updatePatternOption();
 }
 
@@ -61,7 +60,6 @@ const showWeekly = () => {
     dailyTab.classList.remove('active')
     weeklyTab.classList.add('active');
     monthlyTab.classList.remove('active');
-    yearlyTab.classList.remove('active');
     updatePatternOption();
 }
 
@@ -69,7 +67,6 @@ const showMonthly = () => {
     dailyTab.classList.remove('active')
     weeklyTab.classList.remove('active');
     monthlyTab.classList.add('active');
-    yearlyTab.classList.remove('active');
     updatePatternOption();
 }
 
@@ -77,7 +74,6 @@ const showYearly = () => {
     dailyTab.classList.remove('active')
     weeklyTab.classList.remove('active');
     monthlyTab.classList.remove('active');
-    yearlyTab.classList.add('active');
     updatePatternOption();
 }
 
@@ -120,6 +116,7 @@ const getDailyPattern = (endOccurrences, endDate, startDate) => {
 const getWeeklyPattern = (endOccurrences, endDate, startDate) => {
     //WEEKLY CODE
     const weekPattern = document.getElementById('week-pattern'); //weekly recurrence number
+    const occurrenceInput = document.getElementById('occurrence');
     const checkboxSunday = document.getElementById('week-pattern-sunday');
     const checkboxMonday = document.getElementById('week-pattern-monday');
     const checkboxTuesday = document.getElementById('week-pattern-tuesday');
@@ -129,7 +126,7 @@ const getWeeklyPattern = (endOccurrences, endDate, startDate) => {
     const checkboxSaturday = document.getElementById('week-pattern-saturday');
     const weekdayOptions = [checkboxSunday, checkboxMonday, checkboxTuesday, checkboxWednesday, checkboxThursday, checkboxFriday, checkboxSaturday];
     const weekdays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
-    const selectedDays = weekdayOptions.filter((elem, index) => elem.checked).map(elem => weekdays.indexOf(elem.value));
+    const selectedDays = weekdayOptions.filter(elem => elem.checked).map(elem => weekdays.indexOf(elem.value));
 
     console.log(selectedDays)
 
@@ -142,42 +139,66 @@ const getWeeklyPattern = (endOccurrences, endDate, startDate) => {
     let allDays = [];
     let currDate = startDate;
     let currentWeek = []
-
-    while ((days.length < endOccurrences - 1 || new Date(currDate) <= new Date(endDate)) && days.length < eventCreateLimit) {
+    while ((new Date(endDate) >= new Date(currDate) || days.length <= (endOccurrences * selectedDays.length) - 1) && days.length < eventCreateLimit) {
         let tomorrow = new Date(currDate);
         tomorrow.setDate(tomorrow.getDate() + 1);
-        currentWeek.push(tomorrow);
         currDate = tomorrow;
-        if (currDate.getDay() == 0) {
+        
+        if (currDate.getDay() == 0 && currentWeek.length) {
             allDays.push(currentWeek);
-            currentWeek = [];
-        }
-    }
-    console.log(currentWeek)
-    if (currentWeek.length) {
-        allDays.push(currentWeek);
-        currentWeek = [];
-    }
-    
-    for (let i = weekPattern.value - 1; i < allDays.length; i += weekPattern.value) {
-        allDays.splice(i, 1);
-        i --;
-    }
-    
-    for (week of allDays) {
-        for (day of week) {
-            if (selectedDays.includes(day.getDay())) {
-                days.push(day.toISOString())
+            
+            if ((allDays.length - 1) % weekPattern.value == 0) {
+                days = days.concat(currentWeek)
             }
-            console.log(day)
+            currentWeek = [];
+        } else if (new Date(endDate) < currDate) {
+            allDays.push(currentWeek)
+            
+            if ((allDays.length - 1) % weekPattern.value == 0) {
+                days = days.concat(currentWeek)
+            }
         }
+
+        if (selectedDays.includes(currDate.getDay())) {
+            currentWeek.push(currDate.toISOString())
+        }
+
+    }
+    
+    if (occurrenceInput.checked && days.length > endOccurrences * selectedDays.length) {
+        days.length = endOccurrences * selectedDays.length;
+    }
+}
+
+const getWeekdayOfMonth = (dateString, dayOfWeek, weekValue) => {
+    console.log(dateString, dayOfWeek, weekValue)
+    var date = new Date(dateString);
+
+    var day = date.getDay();
+    var diffDays = 0;
+
+    if (day > dayOfWeek) {
+      diffDays = 7 - (day - dayOfWeek);
+    } else {
+      diffDays = dayOfWeek - day
     }
 
-    return true;
+    return new Date(date.setDate(date.getDate() + (diffDays + (7 * (weekValue - 1)))));
 }
-const getMonthlyPattern = () => {
+const getLastWeekdayOfMonth = (year, month, weekday) => {
+    var date = new Date(year, month, 1);
+
+    var days = [];
+
+    while (date.getMonth() === month) {
+        if (date.getDay() === weekday) days.push(new Date(date).toISOString())
+        date.setDate(date.getDate() + 1);
+    }
+
+    return days[days.length -1];
+}
+const getMonthlyPattern = (endOccurrences, endDate, startDate) => {
     //MONTHLY CODE
-    patternType = 2;
     const dayOfMonthOption = document.getElementById('day-of-month');
     const timesOfMonthOption = document.getElementById('times-of-month');
 
@@ -185,39 +206,34 @@ const getMonthlyPattern = () => {
         const monthDayValue = document.getElementById('month-day')
         const everyMonthValue = document.getElementById('every-month-num')
 
-        // console.log(`Day ${monthDayValue.value} of every ${everyMonthValue.value} month(s)`)
-        simplifiedInstructions = `Day ${monthDayValue.value} of every ${everyMonthValue.value} month(s)`;
+        days = [new Date(startDate).toISOString()];
+        let currDate = new Date(new Date(startDate).setDate(monthDayValue.value))
+        while ((currDate < new Date(endDate) || days.length < endOccurrences) && days.length < eventCreateLimit) {
+            console.log(everyMonthValue.value)
+            currDate = new Date(new Date(currDate).setMonth(currDate.getMonth() + parseInt(everyMonthValue.value)))
+            days.push(currDate.toISOString());
+        }
+        console.log(days)
+
     } else if (timesOfMonthOption.checked) {
         const monthDayPattern = document.getElementById('monthly-day-pattern');
         const weekdayPattern = document.getElementById('weekday-pattern');
         const everyMonthValue = document.getElementById('every-month-num-2')
 
-        // console.log(`The ${monthDayPattern.value} ${weekdayPattern.value} of every ${everyMonthValue.value} month(s)`)
-        simplifiedInstructions = `The ${monthDayPattern.value} ${weekdayPattern.value} of every ${everyMonthValue.value} month(s)`;
-    } else {
-        // console.log('missing parameters')
-        // errors.push('missing parameters')
-    }
-}
-const getYearlyPattern = () => {
-    //YEARLY CODE
-    patternType = 3;
-    const singleMonthDay = document.getElementById('single-month-day');
-    const singleMonthTime = document.getElementById('single-month-time');
+        let fullStartDate = new Date(startDate)
 
-    if (singleMonthDay.checked) {
-        const monthYearPattern = document.getElementById('month-year-pattern');
-        const monthPatternNumber = document.getElementById('month-pattern-number');
-
-        // console.log(`Every ${monthYearPattern.value} ${monthPatternNumber.value}`);
-        simplifiedInstructions = `Every ${monthYearPattern.value} ${monthPatternNumber.value}`;
-    } else if (singleMonthTime.checked) {
-        const yearlyDayPattern = document.getElementById('yearly-day-pattern');
-        const yearlyWeekPattern = document.getElementById('yearly-week-pattern');
-        const yearlyMonthPattern = document.getElementById('yearly-month-pattern');
-
-        // console.log(`The ${yearlyDayPattern.value} ${yearlyWeekPattern.value} of ${yearlyMonthPattern.value}`)
-        simplifiedInstructions = `The ${yearlyDayPattern.value} ${yearlyWeekPattern.value} of every ${yearlyMonthPattern.value}`
+        days = [new Date(startDate).toISOString()];
+        //gets the first day of the next month after the start date
+        let currDate = new Date(fullStartDate.getFullYear(), fullStartDate.getMonth() + parseInt(everyMonthValue.value), 1)
+        while ((currDate <= new Date(endDate) || days.length < endOccurrences) && days.length < eventCreateLimit) {
+            let newDate = getWeekdayOfMonth(currDate.toLocaleDateString(), parseInt(weekdayPattern.value), parseInt(monthDayPattern.value))
+            console.log(newDate)
+            days.push(newDate.toISOString().split('T')[0] + 'T' + fullStartDate.toISOString().split('T')[1]);
+            // days.push(currDate)
+            currDate = new Date(currDate.getFullYear(), currDate.getMonth() + parseInt(everyMonthValue.value), 1)
+        }
+        // console.log(days)
+        
     } else {
         // console.log('missing parameters')
         // errors.push('missing parameters')
@@ -236,7 +252,7 @@ const hideWarning = () => {
 const handleSave = () => {
     const activeOption = document.querySelector('.active').id;
 
-    let patternTypes = ['daily', 'weekly', 'monthly', 'yearly'];
+    let patternTypes = ['daily', 'weekly', 'monthly'];
     let patternType = patternTypes.indexOf(activeOption);
 
     //radio buttons
@@ -258,7 +274,7 @@ const handleSave = () => {
 
     switch (patternType) {
         case 0:
-             if (getDailyPattern(occurrenceNumber.value, byDateInputDate.value, startDateDOM.value)) {
+             if (getDailyPattern(occurrenceInput.checked ? occurrenceNumber.value : 0, byDateInput.checked ? byDateInputDate.value : '', startDateDOM.value)) {
                 hideWarning();
              } else {
                 showWarning();
@@ -266,7 +282,7 @@ const handleSave = () => {
              }
             break;
         case 1:
-            if (getWeeklyPattern(occurrenceNumber.value, byDateInputDate.value, startDateDOM.value)) {
+            if (getWeeklyPattern(occurrenceInput.checked ? occurrenceNumber.value : 0, byDateInput.checked ? byDateInputDate.value : '', startDateDOM.value)) {
                 hideWarning();
             } else {
                 showWarning();
@@ -274,39 +290,23 @@ const handleSave = () => {
             }
             break;
         case 2:
-            getMonthlyPattern();
-            break;
-        case 3:
-            getYearlyPattern();
+            if (getMonthlyPattern(occurrenceInput.checked ? occurrenceNumber.value : 0, byDateInput.checked ? byDateInputDate.value : '', startDateDOM.value)) {
+                hideWarning();
+            } else {
+                showWarning();
+                return;
+            }
             break;
         default:
             break;
     }
-
-    // if (activeOption == 'daily') {
-    // } else if (activeOption == 'weekly') {
-    // } else if (activeOption == 'monthly') {
-    // } else if (activeOption == 'yearly') {
-    // }
-
-
-    // let endInstructions = '';
-
-
-    // //if there are no errors
-    // if (!errors.length) {
-    //     warningMsg.style.visibility = 'hidden';
-    //     warningMsg.style.display = 'none';
-
-
-    //     // recurrencePattern = `
-    //     //     start: ${new Date(startDateDOM.value).toDateString()} @ ${new Date(startDateDOM.value).toLocaleTimeString()}
-    //     //     end: ${endInstructions}
-    //     //     recur: ${simplifiedInstructions}
-    //     // `
-    //     // // console.log(recurrencePattern)
-    //     // recurringLabel.innerText = simplifiedInstructions;
-    //     // patternHide();
-    // } else {
-    // }
+}
+function getEveryNth(arr, nth) {
+    const result = [];
+  
+    for (let i = 0; i < arr.length; i += nth) {
+      result.push(arr[i]);
+    }
+  
+    return result;
 }
