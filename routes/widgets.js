83a46affcc82ec3ac87ea42380f36b6a09a7bffe
jblackman42 <jblackman@pureheart.org>
2022-10-03 15:18:59 -0700

    administrator authentication and widget update

--- routes/widgets.js
+++ routes/widgets.js
@@ -2,20 +2,21 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
+const {ensureAdministrator} = require('../middleware/auth.js')
 const StaffSchema = require('../models/Staff');
 
-router.get('/update-staff', async (req, res) => {
+router.get('/update-staff', ensureAdministrator, async (req, res) => {
     res.render('pages/update-staff')
 })
 
-router.delete('/update-staff', async (req, res) => {
+router.delete('/update-staff', ensureAdministrator, async (req, res) => {
     try {
         await StaffSchema.deleteMany({});
         res.status(201).json({msg: 'all entries deleted'})
     } catch (error) { res.status(500).json({ msg: error})}
 })
 
-router.post('/update-staff', async (req, res) => {
+router.post('/update-staff', ensureAdministrator, async (req, res) => {
     try {
         // console.log(staff)
         const staffSection = await StaffSchema.create( req.body )
