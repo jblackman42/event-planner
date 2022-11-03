bc62dfb4cdede0af61d714f95df2e015b1eebec7
jblackman42 <jblackman@pureheart.org>
2022-11-03 09:27:31 -0700

    anti-bot email api

--- routes/widgets.js
+++ routes/widgets.js
@@ -31,148 +31,15 @@ router.get('/staff', async (req, res) => {
     } catch(error) { res.status(500).json({ msg: error }) }
 })
 
-router.get('/webhook-update-staff', ensureWebhook, async (req, res) => {
-    
-    //delete all staff records
-    await StaffSchema.deleteMany({});
+router.get('/staff-email', async (req, res) => {
+    try {
+        const {sectionId, staffId} = req.query;
 
-    //get access token for accessing database informatin
-    const accessToken = await axios({
-        method: 'get',
-        mode: 'cors',
-        url: 'https://phc.events/api/oauth/authorize'
-    })
-        .then(response => response.data.access_token)
-        .catch(err => console.error(err))
-        
-    //2443 - lead pastor
-    //2464 - executive and campus pastors
-    //2387 - lead pastors
-    //2388 - pastors
-    //2389 - directors
-    //2390 - support staff
-    const webStaffGroupIDs = [
-        {id: 2443, title: "Senior Pastor"},
-        {id: 2464, title: "Executive and Campus Pastors"},
-        {id: 2387, title: "Lead Pastors"},
-        {id: 2388, title: "Pastors"},
-        {id: 2389, title: "Directors"},
-        {id: 2390, title: "Support Staff"},
-    ]
-    const getWebStaffGroupParticipants = async (Group_ID) => {
-        return await axios({
-            method: 'get',
-            url: `https://my.pureheart.org/ministryplatformapi/tables/Group_Participants?%24select=Participant_ID&%24filter=Group_ID%3D${Group_ID}%20AND%20End_Date%20IS%20NULL`,
-            mode: 'cors',
-            headers: {
-                Accept: 'application/json',
-                Authorization: `Bearer ${accessToken}`
-            }
-        })
-            .then(response => response.data)
-            .catch(err => console.error(err))
-
-    }
-
-    const getParticipantFromID = async (Participant_ID) => {
-        return await axios({
-            method: 'get',
-            url: `https://my.pureheart.org/ministryplatformapi/tables/Participants/${Participant_ID}`,
-            mode: 'cors',
-            headers: {
-                Accept: 'application/json',
-                Authorization: `Bearer ${accessToken}`
-            }
-        })
-            .then(response => response.data[0])
-            .catch(err => console.error(err))
-    }
-
-    const getContactFromID = async (Contact_ID) => {
-        return await axios({
-            method: 'get',
-            url: `https://my.pureheart.org/ministryplatformapi/tables/Contacts/${Contact_ID}`,
-            mode: 'cors',
-            headers: {
-                Accept: 'application/json',
-                Authorization: `Bearer ${accessToken}`
-            }
-        })
-            .then(response => response.data[0])
-            .catch(err => console.error(err))
-    }
-
-    const getFilesFromContactID = async (Contact_ID) => {
-        return await axios({
-            method: 'get',
-            url: `https://my.pureheart.org/ministryplatformapi/files/Contacts/${Contact_ID}`,
-            mode: 'cors',
-            headers: {
-                Accept: 'application/json',
-                Authorization: `Bearer ${accessToken}`
-            }
-        })
-            .then(response => response.data.filter(file => file.IsDefaultImage)[0])
-            .catch(err => console.error(err))
-    }
-    //link for images
-    //https://my.pureheart.org/ministryplatformapi/files/${FileId}
-
-    const getStaffRecordFromContactID = async (Contact_ID) => {
-        return await axios({
-            method: 'get',
-            url: `https://my.pureheart.org/ministryplatformapi/tables/Staff?%24filter=Contact_ID=${Contact_ID}`,
-            mode: 'cors',
-            headers: {
-                Accept: 'application/json',
-                Authorization: `Bearer ${accessToken}`
-            }
-        })
-            .then(response => response.data[0])
-            .catch(err => console.error(err))
-    }
-
-    const formatStaff = async () => {
-
-        for (let i = 0; i < webStaffGroupIDs.length; i ++) {
-            const {id, title} = webStaffGroupIDs[i];
-            let staffParticipants = [];
-            const participants = await getWebStaffGroupParticipants(id)
-            for (let j = 0; j < participants.length; j ++) {
-
-                const {Contact_ID} = await getParticipantFromID(participants[j].Participant_ID)
-                const contact = await getContactFromID(Contact_ID);
-                const {Nickname, First_Name, Last_Name, Email_Address, Email_Unlisted, Mobile_Phone, Mobile_Phone_Unlisted, Web_Page} = contact;
-                const {Job_Title, Bio} = await getStaffRecordFromContactID(Contact_ID);
-                const {UniqueFileId} = await getFilesFromContactID(Contact_ID);
-                const imageUrl = `https://my.pureheart.org/ministryplatformapi/files/${UniqueFileId}`
-                const display_name = `${Nickname} ${Last_Name}`;
-                const email = Email_Unlisted ? null : Email_Address;
-                const phone = Mobile_Phone_Unlisted ? null : Mobile_Phone;
-                
-                staffParticipants.push({
-                    Contact_ID: Contact_ID,
-                    First_Name: First_Name,
-                    Last_Name: Last_Name,
-                    Display_Name: display_name,
-                    Email: email,
-                    Phone: phone,
-                    Job_Title: Job_Title,
-                    Image_URL: imageUrl,
-                    Web_Page: Web_Page,
-                    Bio: Bio
-                })
-            }
-            await StaffSchema.create( {
-                Group_ID: id,
-                Group_Title: title,
-                Participants: staffParticipants
-            } )
-        }
-        res.sendStatus(200)
-    }
-
-    formatStaff()
+        const allStaff = await StaffSchema.find({});
+        console.log(sectionId, staffId)
+        const Email = allStaff[sectionId].Participants[staffId].Email
+        res.status(201).json({ Email });
+    } catch(error) { console.error(error); res.status(500).json({ msg: error }) }
 })
 
 module.exports = router;
