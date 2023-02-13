6bc17e2f0c3521325d472b4250b70e23f1c0ae5b
jblackman42 <jblackman@pureheart.org>
2023-02-13 15:43:05 -0700

    increased limit more

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const cookieParser = require("cookie-parser");
 const prompt = require('prompt');
@@ -13,8 +12,8 @@ app.use(cookieParser());
 app.use(cors());
 require('dotenv').config();
 
-app.use(bodyParser.json({ limit: '16mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '16mb' }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
