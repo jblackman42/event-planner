b20f2766bb0ad1ca8f8e285463021743e104a178
jblackman42 <jblackman@pureheart.org>
2023-02-14 11:15:44 -0700

    Revert "multipart form handling"
    This reverts commit e8e12131f262362e697a6786110590555c5595dd.

--- routes/helpdesk.js
+++ routes/helpdesk.js
@@ -23,21 +23,26 @@ app.get('/files', async (req, res) => {
 //create image
 app.post('/files', async (req, res) => {
   try {
+    const {body} = req.body;
+    console.log('request body:')
+    console.log(req.body)
+    console.log('body:')
+    console.log(body)
       
-    let filename = req.body.name;
+    let filename = body.name;
     let count = 0;
     const createFileName = async (name) => {
         const match = await db.collection('gridFsEx').findOne({ _id: name });
         if (match) {
             // matching name found
             count ++;
-            await createFileName(`${req.body.name} (${count})`);
+            await createFileName(`${body.name} (${count})`);
         } else {
             filename = name;
         }
     }
     
-    await createFileName(req.body.name);
+    await createFileName(body.name);
     const user = { _id: filename, files: [] };
 
 
@@ -57,7 +62,7 @@ app.post('/files', async (req, res) => {
     }
 
     // console.log(req.files)
-    const {attachments} = req.body;
+    const {attachments} = body;
     createFile(attachments)
 
     await db.collection('gridFsEx').insertOne(user);
@@ -65,8 +70,6 @@ app.post('/files', async (req, res) => {
     res.status(200).send(user);
     // res.sendStatus(200);
   } catch (e) {
-    console.log('something terrible has happened:\n')
-    console.log(e)
     res.status(e.statusCode || 500).json({success: false, msg: e})
   }
 })
