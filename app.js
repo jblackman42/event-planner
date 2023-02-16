12df0db8f03287cc5317244ce20a9f9cf2b02f41
jblackman42 <jblackman@pureheart.org>
2023-02-16 13:24:14 -0700

    added new sermon finder widget api

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const upload = require('express-fileupload');
 
 //middleware
 app.use(upload());
-app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 require('dotenv').config();
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
--- routes/widgets.js
+++ routes/widgets.js
@@ -46,141 +46,80 @@ router.post('/staff', async (req, res) => {
 
 // sermons widget here -------------------------------------------------------------------------------
 
-router.get('/update-sermons', ensureAdministrator, async (req, res) => {
-    res.render('pages/update-sermons')
-})
+router.get('/series', async (req, res) => {
+    let SeriesType = req.query.SeriesType;
+    let SeriesID = req.query.SeriesID;
 
-router.get('/sermons', async (req, res) => {
-    try {
-        const sermons = await SermonSchema.find({});
-        res.status(201).json({ sermons });
-    } catch(error) { res.status(500).json({ msg: error }) }
-})
-router.get('/sermon', async (req, res) => {
-    try {
-        const sermon = await SermonSchema.find({Sermon_Series_ID: req.query.id});
-        res.status(201).json({ sermon });
-    } catch(error) { res.status(500).json({ msg: error }) }
-})
-
-router.delete('/update-sermons', ensureAdministrator, async (req, res) => {
-    try {
-        await SermonSchema.deleteMany({});
-        res.status(201).json({msg: 'all entries deleted'})
-    } catch (error) { res.status(500).json({ msg: error})}
-})
-
-router.post('/update-sermons', ensureAdministrator, async (req, res) => {
-    try {
-        // console.log(staff)
-        const sermonSection = await SermonSchema.create( req.body )
-        res.status(201).json({ sermonSection })
-    } catch (error) { console.log(error);res.status(500).json({ msg: error }) }
-})
-
-router.get('/webhook-update-sermons', ensureWebhook, async (req, res) => {
+    if (!SeriesType) {
+        SeriesType = 1;
+    }
     
-    //delete all staff records
-    await SermonSchema.deleteMany({});
-
-    //get access token for accessing database informatin
     const accessToken = await axios({
-        method: 'get',
-        mode: 'cors',
-        url: 'https://phc.events/api/oauth/authorize'
+        method: 'post',
+        url: 'https://my.pureheart.org/ministryplatformapi/oauth/connect/token',
+        data: qs.stringify({
+            grant_type: "client_credentials",
+            scope: "http://www.thinkministry.com/dataplatform/scopes/all",
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET
+        })
     })
         .then(response => response.data.access_token)
         .catch(err => console.error(err))
-    
-    //get sermon series list
-    const sermons = await axios({
-        url: 'https://my.pureheart.org/ministryplatformapi/tables/Pocket_Platform_Sermon_Series?$filter=Series_Start_Date < GETDATE() AND Sermon_Series_Type_ID = 1&$orderby=Series_Start_Date DESC',
-        method: 'GET',
+
+    const data = await axios({
+        method: 'post',
+        url: `https://my.pureheart.org/ministryplatformapi/procs/api_Widget_GetAllSeries`,
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + accessToken,
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        },
+        data: {
+            "@Sermon_Series_Type_ID": SeriesType,
+            "@Sermon_Series_ID": SeriesID || null
         }
     })
-        .then(response => response.data)
+        .then(response => response.data[0])
+        .catch(err => console.error(err))
+
+    res.status(200).send(data).end();
+})
+
+router.get('/sermons', async (req, res) => {
+    let SeriesID = req.query.SeriesID
+
+    if (!SeriesID) {
+        res.status(403).send({msg: "Procedure or function 'api_Widget_GetSeries' expects parameter '@Sermon_Series_ID', which was not supplied."})
+    }
     
-    for (let i = 0; i < sermons.length; i ++) {
-        //get series image
-        const file = await axios({
-            url: `https://my.pureheart.org/ministryplatformapi/files/Pocket_Platform_Sermon_Series/${sermons[i].Sermon_Series_ID}`,
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + accessToken,
-            }
+    const accessToken = await axios({
+        method: 'post',
+        url: 'https://my.pureheart.org/ministryplatformapi/oauth/connect/token',
+        data: qs.stringify({
+            grant_type: "client_credentials",
+            scope: "http://www.thinkministry.com/dataplatform/scopes/all",
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET
         })
-            .then(response => response.data)
+    })
+        .then(response => response.data.access_token)
+        .catch(err => console.error(err))
 
-        sermons[i].fileId = file[0] ? file[0].UniqueFileId : null;
-        sermons[i].Series_Image = file[0] ? `https://my.pureheart.org/ministryplatformapi/files/${file[0].UniqueFileId}` : null;
-
-        //get messages in series
-        const messages = await axios({
-            url: `https://my.pureheart.org/ministryplatformapi/tables/Pocket_Platform_Sermons?%24filter=Series_ID=${sermons[i].Sermon_Series_ID}`,
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + accessToken,
-            }
-        })
-            .then(response => response.data)
-
-        sermons[i].messages = messages;
-
-        //get watch/listen links
-        for (let j = 0; j < messages.length; j ++) {
-            const links = await axios({
-                url: `https://my.pureheart.org/ministryplatformapi/tables/Pocket_Platform_Sermon_Links?%24filter=Sermon_ID=${messages[j].Sermon_ID}`,
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + accessToken,
-                }
-            })
-                .then(response => response.data)
-            
-            sermons[i].messages[j].links = links;
-
-            const fileId = await axios({
-                url: `https://my.pureheart.org/ministryplatformapi/files/Pocket_Platform_Sermons/${sermons[i].messages[j].Sermon_ID}`,
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + accessToken,
-                }
-            })
-                .then(response => response.data[0]);
-                sermons[i].messages[j].Sermon_Image = fileId ? `https://my.pureheart.org/ministryplatformapi/files/${fileId.UniqueFileId}` : null;
-
-            const speakerName = await axios({
-                url: `https://my.pureheart.org/ministryplatformapi/tables/Pocket_Platform_Speakers/${sermons[i].messages[j].Speaker_ID}`,
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + accessToken,
-                }
-            })
-                .then(response => response.data[0]);
-                sermons[i].messages[j].Speaker = speakerName ? speakerName.Display_Name : null;
+    const data = await axios({
+        method: 'post',
+        url: `https://my.pureheart.org/ministryplatformapi/procs/api_Widget_GetSeries`,
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        },
+        data: {
+            "@Sermon_Series_ID": SeriesID
         }
-        console.log(`${i + 1} / ${sermons.length}`)
-    }
+    })
+        .then(response => response.data[0])
+        .catch(err => console.error(err))
 
-    for (let i = 0; i < sermons.length; i ++) {
-        await SermonSchema.create(sermons[i]);
-        console.log(`${i + 1} / ${sermons.length}`)
-    }
-    res.sendStatus(200)
+    res.status(200).send(data).end();
 })
 
 router.post('/opportunity-auto-place', ensureWebhook, async (req, res) => {
