b9e3ef049f8d91e46660c27be9402d453408c8ed
jblackman42 <jblackman@pureheart.org>
2023-02-27 13:03:42 -0700

    added featured events api

--- routes/prayer-wall.js
+++ routes/prayer-wall.js
@@ -22,47 +22,56 @@ const sendNotifications = async (req, res) => {
         .then(response => response.data)
         .catch(err => console.error(err))
 
-    console.log(prayersToNotify.length)
     for (let i = 0; i < prayersToNotify.length; i ++) {
 
-        const {Author_Name, Author_Email, Prayer_Count} = prayersToNotify[i];
-        //send email to prayer authors letting them know how many times their request has been prayed for
-        await axios({
-            method: 'post',
-            url: 'https://my.pureheart.org/ministryplatformapi/messages',
-            headers: {
-                'content-type': 'application/json',
-                'authorization': `Bearer ${access_token}`
-            },
-            data: {
-                "FromAddress": { "DisplayName": "Prayer Wall", "Address": "noreply@pureheart.org" },
-                "ToAddresses": 
-                [ 
-                    { "DisplayName": Author_Name, "Address": Author_Email }
-                ],
-                "ReplyToAddress": { "DisplayName": "noreply@pureheart.org", "Address": "noreply@pureheart.org" },
-                "Subject": "Prayer Request Update",
-                "Body": `
-                    <p>Dear ${Author_Name},</p>
-                    <p>We wanted to thank you for sharing your prayer request with us on our online prayer wall. We have received your request and added it to our prayer list.</p>
-                    <p>We believe in the power of prayer and want to assure you that we are praying for you. So far, your request has been prayed for <strong>${Prayer_Count} times.</strong></p>
-                    <p>Thank you for trusting us with your prayer request. Our thoughts and prayers are with you.</p>
-                    <p>Sincerely,</p>
-                    <p>Pure Heart Church<br>Prayer Team</p>
-                `
-                // "Body": `Your recent contribution to the Pure Heart Church Prayer Wall has just been prayed for! It has now been prayed for ${Prayer_Count} times.`
-            }
-        })
-        //removes notification scheduled bit field
-        prayersToNotify[i].Notification_Scheduled = 0;
-        await axios({
-            method: 'put',
-            url: 'https://my.pureheart.org/ministryplatformapi/tables/Prayer_Requests',
-            headers: {
-                'Authorization': `Bearer ${access_token}`
-            },
-            data: [prayersToNotify[i]]
-        })
+        try {
+
+            const {Author_Name, Author_Email, Prayer_Count, Prayer_Request_ID} = prayersToNotify[i];
+            console.log(prayersToNotify[i])
+            //send email to prayer authors letting them know how many times their request has been prayed for
+            await axios({
+                method: 'post',
+                url: 'https://my.pureheart.org/ministryplatformapi/messages',
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${access_token}`
+                },
+                data: {
+                    "FromAddress": { "DisplayName": "Prayer Wall", "Address": "noreply@pureheart.org" },
+                    "ToAddresses": 
+                    [ 
+                        { "DisplayName": Author_Name, "Address": Author_Email }
+                    ],
+                    "ReplyToAddress": { "DisplayName": "noreply@pureheart.org", "Address": "noreply@pureheart.org" },
+                    "Subject": "Prayer Request Update",
+                    "Body": `
+                        <p>Dear ${Author_Name},</p>
+                        <p>We wanted to thank you for sharing your prayer request with us on our online prayer wall. We have received your request and added it to our prayer list.</p>
+                        <p>We believe in the power of prayer and want to assure you that we are praying for you. So far, your request has been prayed for <strong>${Prayer_Count} times.</strong></p>
+                        <p>Thank you for trusting us with your prayer request. Our thoughts and prayers are with you.</p>
+                        <p>Sincerely,</p>
+                        <p>Pure Heart Church<br>Prayer Team</p>
+                        <br/>
+                        <br/>
+                        <a href="https://www.pureheart.org/prayerwall-unsubscribe/?id=${Prayer_Request_ID}">unsubscribe</a>
+                    `
+                    // "Body": `Your recent contribution to the Pure Heart Church Prayer Wall has just been prayed for! It has now been prayed for ${Prayer_Count} times.`
+                }
+            })
+            //removes notification scheduled bit field
+            prayersToNotify[i].Notification_Scheduled = 0;
+            await axios({
+                method: 'put',
+                url: 'https://my.pureheart.org/ministryplatformapi/tables/Prayer_Requests',
+                headers: {
+                    'Authorization': `Bearer ${access_token}`
+                },
+                data: [prayersToNotify[i]]
+            })
+
+        } catch (e) {
+            console.log(e)
+        }
     }
     
     if (!req) return console.log(`sent ${prayersToNotify.length} ${prayersToNotify.length == 1 ? 'notification' : 'notifications'}`)
@@ -70,9 +79,9 @@ const sendNotifications = async (req, res) => {
 }
 
 //sends emails out at 5:00PM each day
-// 8AM
-// schedule.scheduleJob('14 0 * *', () => sendNotifications());
-schedule.scheduleJob('8 * * *', () => sendNotifications());
+// 2PM daily
+schedule.scheduleJob('14 0 * *', () => sendNotifications());
+// schedule.scheduleJob('11 * * *', () => sendNotifications());
 
 
 router.get('/send-notifications', async (req, res) => {
@@ -96,14 +105,7 @@ router.get('/', async (req, res) => {
             'Authorization': `Bearer ${access_token}`
         }
     })
-    .then(response => response.data.map(prayer => {
-        if (prayer.Private) {
-            prayer.Author_Email = 'Anonymous';
-            prayer.Author_Name = 'Anonymous';
-            prayer.Author_Phone = 'Anonymous';
-        }
-        return prayer;
-    }))
+    .then(response => response.data)
     .catch(err => console.log(err))
     
     res.status(200).json({prayer_requests}).end();
@@ -123,14 +125,7 @@ router.get('/:id', async (req, res) => {
             'Authorization': `Bearer ${access_token}`
         }
     })
-    .then(response => response.data.map(prayer => {
-        if (prayer.Private) {
-            prayer.Author_Email = 'Anonymous';
-            prayer.Author_Name = 'Anonymous';
-            prayer.Author_Phone = 'Anonymous';
-        }
-        return prayer;
-    })[0])
+    .then(response => response.data[0])
     .catch(err => console.log(err))
     
     res.status(200).json({prayer_request}).end();
