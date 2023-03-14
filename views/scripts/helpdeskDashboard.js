a30b65707e2f372b5029f4338f90c3328e7d96cc
jblackman42 <jblackman@pureheart.org>
2023-03-14 10:53:10 -0700

    quit pushing

--- views/scripts/helpdeskDashboard.js
+++ views/scripts/helpdeskDashboard.js
@@ -39,8 +39,8 @@ class Dashboard extends HTMLElement {
   }
 
   createWebsocket = () => {
-    // this.webSocket = new WebSocket(`wss://phc.events/websocket`);
-    this.webSocket = new WebSocket(`ws://localhost:3000/websocket`);
+    this.webSocket = new WebSocket(`wss://phc.events/websocket`);
+    // this.webSocket = new WebSocket(`ws://localhost:3000/websocket`);
     
     this.webSocket.addEventListener("open", () => {
       console.log("We are connected");
