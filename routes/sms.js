7ff498e3b2c4b746de6447898b7832b9cd0d398b
jblackman42 <jblackman@pureheart.org>
2023-03-27 13:51:47 -0700

    Update sms.js

--- routes/sms.js
+++ routes/sms.js
@@ -50,10 +50,10 @@ router.post('/reply', (req, res) => {
   // const twiml = new MessagingResponse();
   // twiml.message('The Robots are coming! Head for the hills!');
 
-  // res.type('text/xml').send(twiml.toString());
-
+  
   console.log(Body)
-  res.sendStatus(200);
+  // res.sendStatus(200);
+  res.type('text/xml').send("");
 
 })
 
